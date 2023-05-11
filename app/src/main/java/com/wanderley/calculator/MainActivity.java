4d8b43a523c305b20062b978814f6af9bdf62dbe
Wanderley Cabral da Silva <wcabraldasilvac@gmail.com>
2023-05-11 18:21:13 -0300

    :wrench: chore: Create Method Start Components

--- app/src/main/java/com/wanderley/calculator/MainActivity.java
+++ app/src/main/java/com/wanderley/calculator/MainActivity.java
@@ -11,8 +11,7 @@ import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button numeroZero,numeroUm,numeroDois,numeroTres,numeroQuatro,numeroCinco,numeroSeis,numeroSete,numeroOito,numeroNove,
-    ponto,soma,subtracao,multiplicacao,divisao,igual,botao_limpar;
+    private Button numeroZero,numeroUm,numeroDois,numeroTres,numeroQuatro,numeroCinco,numeroSeis,numeroSete,numeroOito,numeroNove, numeroPonto,soma,subtracao,multiplicacao,divisao,igual,botao_limpar;
 
     private TextView txtExpresao, txtResultado;
     private ImageView backspace;
@@ -23,5 +22,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
        // Objects.requireNonNull(getSupportActionBar()).hide();
+        IniciarComponentes();
     }
+
+    private void IniciarComponentes(){     // Criar Método para chamar, recuperar os id's os componentes
+        numeroZero = findViewById(R.id.numero_zero);
+        numeroUm = findViewById(R.id.numero_um);
+        numeroDois = findViewById(R.id.numero_dois);
+        numeroTres = findViewById(R.id.numero_tres);
+        numeroQuatro = findViewById(R.id.numero_quatro);
+        numeroCinco = findViewById(R.id.numero_cinco);
+        numeroSeis = findViewById(R.id.numero_seis);
+        numeroSete = findViewById(R.id.numero_sete);
+        numeroOito = findViewById(R.id.numero_oito);
+        numeroNove = findViewById(R.id.numero_nove);
+        numeroPonto = findViewById(R.id.numero_ponto;
+        soma = findViewById(R.id.bt_soma);
+        subtracao = findViewById(R.id.bt_subtracao);
+        multiplicacao = findViewById(R.id.bt_multiplicacao);
+        divisao = findViewById(R.id.bt_divisao);
+        botao_limpar = findViewById(R.id.bt_limpar);
+        txtExpresao = findViewById(R.id.txt_expressao);
+        txtResultado = findViewById(R.id.txt_resultado);
+        backspace = findViewById(R.id.backspace);
+
+    }
+
 }

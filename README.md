e02d3fdbe66c5b2f44916628e11293a45c2df2b3
Wanderley Cabral da Silva <wcabraldasilvac@gmail.com>
2023-05-11 17:55:55 -0300

    :wrench: chore: Create calculator objects

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# calculator
+# Calculator Application created with Android Studio.
+## The language used for development was JAVA
+
+
+:ok_hand: docs: Add README file
+:ok_hand: style: calculator layout completion
+:ok_hand: style: Code review changes
+:ok_hand: style: create Layout main
+:ok_hand: style: create styles
+:tada: init: create project Calculator
+
--- app/src/main/java/com/wanderley/calculator/MainActivity.java
+++ app/src/main/java/com/wanderley/calculator/MainActivity.java
@@ -3,10 +3,19 @@ package com.wanderley.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button numeroZero,numeroUm,numeroDois,numeroTres,numeroQuatro,numeroCinco,numeroSeis,numeroSete,numeroOito,numeroNove,
+    ponto,soma,subtracao,multiplicacao,divisao,igual,botao_limpar;
+
+    private TextView txtExpresao, txtResultado;
+    private ImageView backspace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
